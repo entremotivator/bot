130675dd847230209878eed9ddbeb170bddef1c9
entremotivator <104661532+entremotivator@users.noreply.github.com>
2023-12-05 23:24:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -43,8 +43,9 @@ embedding_function = OpenAIEmbeddingFunction(api_key=os.environ.get('OPENAI_API_
 chroma_client = chromadb.PersistentClient(path="./chromadb")
 vector_store = chroma_client.get_or_create_collection(name="reportcards",
                                                       embedding_function=embedding_function)
-
-# Create RAG model and set up TruCustomApp
+vector_store.add(“reportcard_info", documents=GPT_reportcard)
+                 
+                 # Create RAG model and set up TruCustomApp
 os.environ["OPENAI_API_KEY"] = "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"
 oai_client = OpenAI(api_key="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")  # Pass the API key directly
 
