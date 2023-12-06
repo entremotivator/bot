975e1d9b4bb810763b427973c90fccaf5f765388
entremotivator <104661532+entremotivator@users.noreply.github.com>
2023-12-06 01:22:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,29 +7,17 @@ from langchain.llms import GooglePalm
 from langchain.vectorstores import FAISS
 from langchain.chains import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
-from trulens_eval import Tru, TruCustomApp, Feedback, Select
-from trulens_eval.feedback import Groundedness
-from trulens_eval.feedback.provider.openai import OpenAI as fOpenAI
-import numpy as np
 import os
 
-os.environ['GOOGLE_API_KEY'] = 'AIzaSyAANEPA1UF6WE4O_0GQh2s27iBT4VrN0Ag'
+os.environ['GOOGLE_API_KEY'] =  'AIzaSyAANEPA1UF6WE4O_0GQh2s27iBT4VrN0Ag'
 
-# Streamlit settings
-st.set_page_config("Chat with Multiple PDFs")
-st.header("Chat with Multiple PDF 💬")
-
-# Trulens setup
-tru = Tru()
-
-# PDF processing functions
 def get_pdf_text(pdf_docs):
-    text = ""
+    text=""
     for pdf in pdf_docs:
-        pdf_reader = PdfReader(pdf)
+        pdf_reader= PdfReader(pdf)
         for page in pdf_reader.pages:
-            text += page.extract_text()
-    return text
+            text+= page.extract_text()
+    return  text
 
 def get_text_chunks(text):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
@@ -42,8 +30,8 @@ def get_vector_store(text_chunks):
     return vector_store
 
 def get_conversational_chain(vector_store):
-    llm = GooglePalm()
-    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+    llm=GooglePalm()
+    memory = ConversationBufferMemory(memory_key = "chat_history", return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=vector_store.as_retriever(), memory=memory)
     return conversation_chain
 
@@ -51,63 +39,33 @@ def user_input(user_question):
     response = st.session_state.conversation({'question': user_question})
     st.session_state.chatHistory = response['chat_history']
     for i, message in enumerate(st.session_state.chatHistory):
-        if i % 2 == 0:
+        if i%2 == 0:
             st.write("Human: ", message.content)
         else:
             st.write("Bot: ", message.content)
+def main():
+    st.set_page_config("Chat with Multiple PDFs")
+    st.header("Chat with Multiple PDF 💬")
+    user_question = st.text_input("Ask a Question from the PDF Files")
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chatHistory" not in st.session_state:
+        st.session_state.chatHistory = None
+    if user_question:
+        user_input(user_question)
+    with st.sidebar:
+        st.title("Settings")
+        st.subheader("Upload your Documents")
+        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Process Button", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                raw_text = get_pdf_text(pdf_docs)
+                text_chunks = get_text_chunks(raw_text)
+                vector_store = get_vector_store(text_chunks)
+                st.session_state.conversation = get_conversational_chain(vector_store)
+                st.success("Done")
 
-# Streamlit UI
-user_question = st.text_input("Ask a Question from the PDF Files")
-if "conversation" not in st.session_state:
-    st.session_state.conversation = None
-if "chatHistory" not in st.session_state:
-    st.session_state.chatHistory = None
-if user_question:
-    user_input(user_question)
-with st.sidebar:
-    st.title("Settings")
-    st.subheader("Upload your Documents")
-    pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Process Button", accept_multiple_files=True)
-    if st.button("Process"):
-        with st.spinner("Processing"):
-            raw_text = get_pdf_text(pdf_docs)
-            text_chunks = get_text_chunks(raw_text)
-            vector_store = get_vector_store(text_chunks)
-            st.session_state.conversation = get_conversational_chain(vector_store)
-            st.success("Done")
 
-# Trulens integration
-class RAG_from_scratch:
-    # ... (your existing RAG_from_scratch class)
-fopenai = fOpenAI()
-grounded = Groundedness(groundedness_provider=fopenai)
-f_groundedness = (
-    Feedback(grounded.groundedness_measure_with_cot_reasons, name="Groundedness")
-    .on(Select.RecordCalls.retrieve.rets.collect())
-    .on_output()
-    .aggregate(grounded.grounded_statements_aggregator)
-)
 
-f_qa_relevance = (
-    Feedback(fopenai.relevance_with_cot_reasons, name="Answer Relevance")
-    .on(Select.RecordCalls.retrieve.args.query)
-    .on_output()
-)
-
-f_context_relevance = (
-    Feedback(fopenai.qs_relevance_with_cot_reasons, name="Context Relevance")
-    .on(Select.RecordCalls.retrieve.args.query)
-    .on(Select.RecordCalls.retrieve.rets.collect())
-    .aggregate(np.mean)
-)
-
-rag = RAG_from_scratch()
-tru_rag = TruCustomApp(rag,
-                      app_id='RAG v1',
-                      feedbacks=[f_groundedness, f_qa_relevance, f_context_relevance])
-
-with tru_rag as recording:
-    rag.query("When was the University of Washington founded?")
-
-st.write(tru.get_leaderboard(app_ids=["RAG v1"]))
-tru.run_dashboard()
+if __name__ == "__main__":
+    main()
