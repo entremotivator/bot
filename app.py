3dba827a8edc2d936ee2a81564a9aceb5bde9108
entremotivator <104661532+entremotivator@users.noreply.github.com>
2023-12-06 00:51:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ f_context_relevance = (
 
 # Create RAG model and set up TruCustomApp
 os.environ["OPENAI_API_KEY"] = "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"
-oai_client = OpenAI(api_key="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")  # Pass the API key directly
+# Pass the API key directly
 # Assuming the rest of your code remains the same...
 
 # Wrap the custom RAG with TruCustomApp
