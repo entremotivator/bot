3bc81544499ebc341b0614fb6fb4b050270df4d6
entremotivator <104661532+entremotivator@users.noreply.github.com>
2023-12-06 02:25:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,77 +1,73 @@
 import streamlit as st
-from PyPDF2 import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-import google.generativeai as palm
-from langchain.embeddings import GooglePalmEmbeddings
-from langchain.llms import GooglePalm
-from langchain.vectorstores import FAISS
-from langchain.chains import ConversationalRetrievalChain
-from langchain.memory import ConversationBufferMemory
-import os
+import pandas as pd
+import json
+from backend.functions import *
 
-os.environ['GOOGLE_API_KEY'] =  'AIzaSyAANEPA1UF6WE4O_0GQh2s27iBT4VrN0Ag'
+# page setup
+st.set_page_config(
+    page_title="Movie Recommendation With PaLM-2",
+    page_icon="🤖",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
 
-def get_pdf_text(pdf_docs):
-    text = ""
-    try:
-        for pdf in pdf_docs:
-            pdf_reader = PdfReader(pdf)
-            for page in pdf_reader.pages:
-                text += page.extract_text()
-    except PdfReadError as e:
-        st.error(f"Error reading PDF {pdf}: {e}")
-    except Exception as e:
-        st.error(f"An unexpected error occurred: {e}")
-    return text
+# sidebar
+with st.sidebar:
+    st.title("🍿 Movie Recommendation With PaLM-2")
+    st.sidebar.caption("MIT License © 2023 keanteng")
+    with st.expander("PaLM-2 API", expanded=True):
+        api_toggle = st.toggle("Enable PaLM-2 API", value=False)
+        api_input = st.text_input(
+            "PaLM-2 API Token",
+            type="password",
+            placeholder="Enter your PaLM-2 API token here",
+        )
 
-def get_text_chunks(text):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
-    chunks = text_splitter.split_text(text)
-    return chunks
 
-def get_vector_store(text_chunks):
-    embeddings = GooglePalmEmbeddings()
-    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
-    return vector_store
+# main page
 
-def get_conversational_chain(vector_store):
-    llm=GooglePalm()
-    memory = ConversationBufferMemory(memory_key = "chat_history", return_messages=True)
-    conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=vector_store.as_retriever(), memory=memory)
-    return conversation_chain
+## load data
+movie_data = pd.read_excel("data.xlsx")
 
-def user_input(user_question):
-    response = st.session_state.conversation({'question': user_question})
-    st.session_state.chatHistory = response['chat_history']
-    for i, message in enumerate(st.session_state.chatHistory):
-        if i%2 == 0:
-            st.write("Human: ", message.content)
+## configure API
+if api_toggle:
+    api_configure(api_key=api_input)
+else:
+    api_configure(api_key=PALM_TOKEN)
+
+# chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        if is_valid_json(message["content"]) == True:
+            temp = message["content"]
+            temp = json_to_frame(message["content"])
+            st.dataframe(temp, hide_index=True)
         else:
-            st.write("Bot: ", message.content)
-def main():
-    st.set_page_config("Chat with Multiple PDFs")
-    st.header("Chat with Multiple PDF 💬")
-    user_question = st.text_input("Ask a Question from the PDF Files")
-    
-    if "conversation" not in st.session_state or st.session_state.conversation is None:
-        st.session_state.conversation = None
-        st.session_state.chatHistory = None
+            st.markdown(message["content"])
 
-    if user_question:
-        user_input(user_question)
+# chatbot
+prompt = st.chat_input("Enter your prompt here")
 
-    with st.sidebar:
-        st.title("Settings")
-        st.subheader("Upload your Documents")
-        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Process Button", accept_multiple_files=True)
+## workflow
+model = load_llm()
+movie_data = data_processing(movie_data)
 
-        if st.button("Process"):
-            with st.spinner("Processing"):
-                raw_text = get_pdf_text(pdf_docs)
-                text_chunks = get_text_chunks(raw_text)
-                vector_store = get_vector_store(text_chunks)
-                st.session_state.conversation = get_conversational_chain(vector_store)
-                st.success("Done")
+with st.chat_message(name="AI", avatar="🎬"):
+    st.write(
+        "Share your thoughts on a movie you like, and I'll recommend you a movie you might like!"
+    )
 
-if __name__ == "__main__":
-    main()
+if prompt:
+    # Display user message in chat message container
+    st.chat_message("user").markdown(prompt)
+    to_llm = prompt_processing(prompt, movie_data)
+    response = llm_agent(prompt=to_llm, model=model)
+    response_df = json_to_frame(response)
+    st.chat_message("assistant").dataframe(response_df, hide_index=True)
+
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.session_state.messages.append({"role": "assistant", "content": response})
